f7ee2a13d0e7c34599e83928c02370a7ae711a2c
zipperr <zipperr@i.softbank.jp>
2019-11-13 15:46:54 +0900

    Update

--- uma_scraping.py
+++ uma_scraping.py
@@ -8,9 +8,19 @@ import sys
 import glob
 import time
 
-url = 'https://race.netkeiba.com/?pid=race&id=p200901010102'
-filename = '200901010102.csv'
+url = 'https://race.netkeiba.com/?pid=race&id=p'
+id = '20090101'
 
-html = urllib.request.urlopen(url).read()
-df = pandas.read_html(html)[0]
-df.to_csv(filename, index=False)
+filename = '200901010102.csv'
+df = pandas.DataFrame()
+df_all = pandas.DataFrame()
+
+for i in range(10, 20):
+    try:
+        html = urllib.request.urlopen(url + str(id) + str(i) + '02').read()
+        df = pandas.read_html(html)[0]
+        df_all = df_all.append(df)
+    except Exception as e:
+        print(e)
+        continue
+df_all.to_csv(filename, index=False)
